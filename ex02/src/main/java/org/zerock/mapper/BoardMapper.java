a70da5d990703b324b569a26a98920bd87eb9cf0
anseunghun <size411@naver.com>
2021-11-28 19:00:52 +0900

    CR Query & Test Code

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -7,4 +7,10 @@ import org.zerock.domain.BoardVO;
 public interface BoardMapper {
 	// @Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
 }
--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -3,8 +3,10 @@ package org.zerock.mapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.CommandLinePropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -22,8 +24,37 @@ public class BoardMapperTests {
 		log.info("mapper is : " + mapper + "");
 	}
 	
-	@Test
+//	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board + ""));
+	}
+	
+//	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("New Title");
+		board.setContent("New Content");
+		board.setWriter("newbie");
+		
+		mapper.insert(board);
+		log.info(board+"");
+	}
+	
+//	@Test
+	public void testInsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("New Title SelectKey");
+		board.setContent("New Content SelectKey");
+		board.setWriter("newbie");
+		
+		mapper.insertSelectKey(board);
+		log.info(board+"");
+	}
+	
+	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(7L);
+		
+		log.info(board + "");
 	}
 }
