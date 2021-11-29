dc613867a767b2dd8fd50f2d483a02502014a4c1
anseunghun <size411@naver.com>
2021-11-29 23:16:28 +0900

    Business Layer Setting

--- ex02/src/main/java/org/zerock/config/RootConfig.java
+++ ex02/src/main/java/org/zerock/config/RootConfig.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -13,6 +14,7 @@ import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @MapperScan(basePackages = {"org.zerock.mapper"})
+@ComponentScan(basePackages = "org.zerock.service")
 public class RootConfig {
 	
 	@Bean
