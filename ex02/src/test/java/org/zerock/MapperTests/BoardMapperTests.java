9401c7c9b034500b03662d49ff2fbf74550aae3f
anseunghun <size411@naver.com>
2021-12-04 22:59:12 +0900

    Basic DB CRUD Test Code & Commit before Implements Business layer

---
+++ ex02/src/test/java/org/zerock/MapperTests/BoardMapperTests.java
@@ -0,0 +1,83 @@
+package org.zerock.MapperTests;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {org.zerock.config.RootConfig.class})
+@Slf4j
+public class BoardMapperTests {
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+	
+//	@Test
+	public void testExist() {
+		assertNotNull(mapper);
+		
+		log.info("mapper : " + mapper);
+	}
+	
+//	@Test
+	public void tesGetList() {
+		mapper.getList().forEach(board -> log.info(board.toString()));
+	}
+	
+//	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("NEW TITLE");
+		board.setContent("NEW CONTENT");
+		board.setWriter("NEWBIE");
+		
+		mapper.insert(board);
+		
+		log.info(board.toString());
+	}
+	
+//	@Test
+	public void testInsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("NEW TITLE SELECT KEY");
+		board.setContent("NEW CONTENT SELECT KEY");
+		board.setWriter("NEWBIE");
+		
+		mapper.insertSelectKey(board);
+		
+		log.info(board.toString());
+	}
+	
+//	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(7L);
+		
+		log.info(board.toString());
+	}
+	
+//	@Test
+	public void testDelete() {
+		log.info("DELETE COUNT: " + mapper.delete(2L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		
+		board.setBno(3L);
+		board.setTitle("UPDATE TITLE");
+		board.setContent("UPDATE CONTENT");
+		board.setWriter("UPDATE NEWBIE");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT: " + count);
+	}
+}
