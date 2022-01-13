e156ffd58b14c2bc2cec827ec5ab977ca406f03d
anseunghun <size411@naver.com>
2022-01-13 19:18:01 +0900

    RestController Parameter

--- ex03/src/main/java/org/zerock/controller/SampleController.java
+++ ex03/src/main/java/org/zerock/controller/SampleController.java
@@ -10,9 +10,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.SampleVO;
+import org.zerock.domain.Ticket;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -61,5 +65,19 @@ public class SampleController {
 		else result = ResponseEntity.status(HttpStatus.OK).body(vo);
 		
 		return result;
+	}
+	
+	@GetMapping("/product/{cat}/{pid}")
+	public String[] getPath(
+			@PathVariable("cat") String cat,
+			@PathVariable("pid") Integer pid) {
+		return new String[] {"category: " + cat, "productid: " + pid};
+	}
+	
+	@PostMapping("/ticket")
+	public Ticket convert(@RequestBody Ticket ticket) {
+		log.info("conver ticket : " + ticket);
+		
+		return ticket;
 	}
 }
