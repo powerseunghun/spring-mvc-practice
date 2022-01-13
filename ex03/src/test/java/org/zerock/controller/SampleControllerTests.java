5119bb51d9e1900f2f74f07731237b765dd76dc2
anseunghun <size411@naver.com>
2022-01-13 19:44:03 +0900

    Use JavaConfiguration Setting

---
+++ ex03/src/test/java/org/zerock/controller/SampleControllerTests.java
@@ -0,0 +1,9 @@
+package org.zerock.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SampleControllerTests {
+	
+}
